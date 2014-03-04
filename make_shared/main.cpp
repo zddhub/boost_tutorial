9b233e83ce6135363c119dc8fb1cb6f997d0faf3
zdd <zddhub@gmail.com>
2014-03-04 11:34:21 +0800

    make shared patch

--- make_shared/main.cpp
+++ make_shared/main.cpp
@@ -30,8 +30,9 @@ protected:
 
 class SubClass : public Base {
 public:
-    SubClass(const std::string& name ): Base(name) {
+    SubClass(const std::string& name, uint x = 0): Base(name) {
         cout << _name.c_str() << " : SubClass()" <<endl;
+        cout << "x = " << x << endl;
     }
 
     virtual ~SubClass() {
@@ -60,6 +61,9 @@ int main()
     //time: sp2 > sp3
     boost::shared_ptr<SubClass> sp2 = boost::shared_ptr<SubClass>(new SubClass("SubClass 2"));
     boost::shared_ptr<SubClass> sp3 = boost::make_shared<SubClass>("SubClass 3");
+
+    //make_shared limited your to a maximux of 9 arguments
+    boost::shared_ptr<SubClass> sp4 = boost::make_shared<SubClass>("SubClass 4", 4);
 
     sp2->toString();
     sp3->toString();
